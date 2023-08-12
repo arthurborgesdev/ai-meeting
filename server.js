60aeb4ca2f6497787be28ecb0f4354f4539dfbc7
Arthur Borges <arthurborges.dev@gmail.com>
2023-08-12 12:00:09 -0300

    feat: All three AI personas are chatting and interacting withing each other
    However, it appears that the conversation need to be faked, pretending that
    there is a user initiating it.

--- server.js
+++ server.js
@@ -6,15 +6,75 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function aiPersonaChat() {
-    const openai = new OpenAIApi(configuration);
-
-    const chatCompletion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "Hello world"}],
-    });
-    
-    console.log(chatCompletion.data.choices[0].message);
+async function aiChat(persona, messages) {
+  try {
+    const inject = {
+      model: "gpt-4-0613",
+      messages: [persona, ...messages],
+    }
+    // console.log(inject);
+    const chatCompletion = await openai.createChatCompletion(inject);
+    return chatCompletion.data.choices[0].message;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
 }
 
-aiPersonaChat();
+const doctorPersona = {
+  role: "user",
+  content: `You are a doctor, you give medical advices and opinions.
+  You are pragmatic, ethical and always care for others well being. 
+  Look at all the conversation above yours and formulate a brief (max 30 words) to continue the conversation.
+  Remember to introduce yourself and your role on each conversation`,
+  name: "Carlos",
+}
+
+
+const engineerPersona = {
+  role: "user",
+  content: `You are an engineer, you give technical and scientific advices and opinions.
+  You are work-driven, perfectionist and care about the future of humanity.
+  Look at all the conversation above yours and formulate a brief (max 30 words) to continue the conversation.
+  Remember to introduce yourself and your role on each conversation`,
+  name: "Andrea",
+}
+
+
+const lawyerPersona = {
+  role: "user",
+  content: `You are a lawyer, you give legal advices and opinions.
+  You are rational, logical and always care for the truth.
+  Look at all the conversation above yours and formulate a brief (max 30 words) to continue the conversation.
+  Remember to introduce yourself and your role on each conversation`,
+  name: "Jeremia",
+}
+
+const openai = new OpenAIApi(configuration);
+
+messagePool = [];
+
+let message = [];
+
+async function init() {
+  while(true) {
+    message = await aiChat(doctorPersona, messagePool);
+    messagePool.push(message);
+    console.log(message);
+  
+    message = await aiChat(engineerPersona, messagePool);
+    messagePool.push(message);
+    console.log(message);
+  
+    message = await aiChat(lawyerPersona, messagePool);
+    messagePool.push(message);
+    console.log(message);
+  }
+}
+
+init();
+
